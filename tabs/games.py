e3809043ce70e58e89a7b1f151839b6b64806e0b
Cooper Anderson <cooper@cooperstuff.ninja>
2019-02-26 23:22:27 -0800

    Fixed typo

--- tabs/games.py
+++ tabs/games.py
@@ -5,8 +5,8 @@ import braille
 import dithering
 import puzzle
 
-games = ["2D Rubics Cube", "Dithering", "Braille"]
-descriptions = {"2D Rubics Cube": "Slide letters and numbers to reorganize", "Dithering": "Render geometry with dithering", "Braille": "Learn Braille"}
-executables = {"2D Rubics Cube": puzzle.Puzzle, "Dithering": dithering.Braille, "Braille": braille.Braille}
+games = ["2D Rubik's Cube", "Dithering", "Braille"]
+descriptions = {"2D Rubik's Cube": "Slide letters and numbers to reorganize", "Dithering": "Render geometry with dithering", "Braille": "Learn Braille"}
+executables = {"2D Rubik's Cube": puzzle.Puzzle, "Dithering": dithering.Braille, "Braille": braille.Braille}
 games.sort()
 
