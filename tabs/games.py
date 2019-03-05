f1960fbb4edd128f5bcb49933366389dd60a4bef
Cooper Anderson <cooper@cooperstuff.ninja>
2019-03-05 02:11:52 -0500

    Add new game: BPuzzle

--- tabs/games.py
+++ tabs/games.py
@@ -4,9 +4,10 @@
 import braille
 import dithering
 import puzzle
+import bpuzzle
 
-games = ["2D Rubik's Cube", "Dithering", "Braille"]
-descriptions = {"2D Rubik's Cube": "Slide letters and numbers to reorganize", "Dithering": "Render geometry with dithering", "Braille": "Learn Braille"}
-executables = {"2D Rubik's Cube": puzzle.Puzzle, "Dithering": dithering.Braille, "Braille": braille.Braille}
+games = ["2D Rubik's Cube", "Dithering", "Braille", "BPuzzle"]
+descriptions = {"2D Rubik's Cube": "Slide letters and numbers to reorganize", "Dithering": "Render geometry with dithering", "Braille": "Learn Braille", "BPuzzle": "The 2D Rubik's Cube but with Braille letters"}
+executables = {"2D Rubik's Cube": puzzle.Puzzle, "Dithering": dithering.Braille, "Braille": braille.Braille, "BPuzzle": bpuzzle.BPuzzle}
 games.sort()
 
